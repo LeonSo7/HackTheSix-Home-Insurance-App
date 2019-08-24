d8e8cde3f31a20f0aad046dad9783f6a58421006
Leon <leon_so7@hotmail.com>
2019-08-24 10:42:27 -0400

    Changes

--- client/homeInsuranceApp/App.js
+++ client/homeInsuranceApp/App.js
@@ -1,62 +1,57 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+// @flow
+import React from "react";
+import { StatusBar, StyleSheet, View } from "react-native";
+import { Container } from "native-base";
+// import { Provider } from "react-redux";
+import { AppLoading, Asset, Font, Icon } from "expo";
+import AppNavigator from "../navigation/AppNavigator";
+// import store from "./src/redux/store";
+// import Sentry from "sentry-expo";
 
-import AppNavigator from './navigation/AppNavigator';
+// Sentry.config(
+//   "https://0c78b61c4982412682614032231d5d82@sentry.io/1340013"
+// ).install();
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+export default class App extends React.Component() {
+  state = {
+    isLoadingComplete: false
+  };
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        // <Provider store={store}>
+          <Container style={styles.container}>
+            <StatusBar />
+            <AppNavigator />
+          </Container>
+        // </Provider>
+      );
+    }
   }
-}
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
+  _handleLoadingError = error => {
+    /* In this case, you might want to report the error to your error
+      reporting service, for example Sentry */
+    console.warn(error);
+  };
 
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
