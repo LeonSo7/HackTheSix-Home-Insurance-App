8c35186c1e819d8f873161bdc2bafea33e8c96a2
Joy Xiao <xiaoz18@mcmaster.ca>
2019-08-24 16:16:30 -0400

    added image to home page

--- client/homeInsureApp/screens/HomeScreen.js
+++ client/homeInsureApp/screens/HomeScreen.js
@@ -16,23 +16,31 @@ import { MonoText } from "../components/StyledText";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <View>
-        <TouchableOpacity style={styles.customBtnBG1} onPress={() => {}}>
-          <Text style={styles.customBtnText}>Button 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.customBtnBG1} onPress={() => {}}>
-          <Text style={styles.customBtnText}>Button 2</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View>
-        <TouchableOpacity style={styles.customBtnBG1} onPress={() => {}}>
-          <Text style={styles.customBtnText}>Button 3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.customBtnBG1} onPress={() => {}}>
-          <Text style={styles.customBtnText}>Button 4</Text>
-        </TouchableOpacity>
+    <View>
+      <Image
+        style={{
+          width: "100%",
+          height: (Dimensions.get("window").height * 4) / 10
+        }}
+        source={require("../assets/images/WavyBlue.jpg")}
+      />
+      <View style={styles.buttons}>
+        <View>
+          <TouchableOpacity style={styles.customBtnBG1} onPress={() => {}}>
+            <Text style={styles.customBtnText}>Button 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.customBtnBG1} onPress={() => {}}>
+            <Text style={styles.customBtnText}>Button 2</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity style={styles.customBtnBG1} onPress={() => {}}>
+            <Text style={styles.customBtnText}>Button 3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.customBtnBG1} onPress={() => {}}>
+            <Text style={styles.customBtnText}>Button 4</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -42,49 +50,30 @@ HomeScreen.navigationOptions = {
   header: null
 };
 
-// function DevelopmentModeNotice() {
-// if (__DEV__) {
-//   const learnMoreButton = (
-//     <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-//       Learn more
-//     </Text>
-//   );
-//   return (
-//     <Text style={styles.developmentModeText}>
-//       Development mode is enabled: your app will be slower but you can use
-//       useful development tools. {learnMoreButton}
-//     </Text>
-//   );
-// } else {
-//   return (
-//     <Text style={styles.developmentModeText}>
-//       You are not in development mode: your app will run at full speed.
-//     </Text>
-//   );
-// }
-// }
-
 const styles = StyleSheet.create({
-  container: {
+  buttons: {
     flex: 1,
     flexDirection: "row",
     backgroundColor: "#fff",
-    marginTop: "20%",
-    marginBottom: 0,
-    height: Dimensions.get("window").height
+    margin: "2%",
+    // marginTop: (Dimensions.get("window").height * 4) / 10,
+    height: (Dimensions.get("window").height * 95) / 100,
+    width: Dimensions.get("window").width - 2
   },
   customBtnText: {
     fontSize: 25,
     fontWeight: "400",
     textAlign: "center",
-    color: "#fff"
+    color: "#1A90F5"
   },
   customBtnBG1: {
-    backgroundColor: "#007aff",
+    backgroundColor: "#F5F5F5",
     borderRadius: 5,
-    width: "45%",
+    width: (Dimensions.get("window").width * 9) / 20,
     marginRight: "2%",
     marginLeft: "2%",
-    height: "60%"
+    marginBottom: "2%",
+    marginTop: "5%",
+    height: (Dimensions.get("window").height - 1) / 4
   }
 });
